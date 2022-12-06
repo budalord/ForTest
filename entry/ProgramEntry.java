1134ca3f7149dda6f4d2bfbc908af2e53677c58e
budalord <1632108814@qq.com>
2022-12-06 22:47:18 +0800

    entry

---
+++ entry/ProgramEntry.java
@@ -0,0 +1,11 @@
+package entry;
+
+import gui.WindowsControl;
+
+public class ProgramEntry {
+    public static void main(String[] args) {
+        WindowsControl windows=new WindowsControl();
+        windows.mainWindows();
+
+    }
+}
